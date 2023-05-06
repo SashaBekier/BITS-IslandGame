a02a6728a59dea6c6ab39b9f8653389c45b0238a
John Burkett <john.burkett2@gmail.com>
2023-05-07 01:01:54 +1000

    Basic Select Screen, One button
    -TextMesh Pro added for UI text (most of the asset nonsense in this commit).
    -HighTower font added.
    -Basic Select Screen plus one button

---
+++ Assets/Scripts/CharacterSelector.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelector : MonoBehaviour
+{
+    // First spawn player prefab
+    public GameObject player;
+
+    //Review: Player Position may already be in other location
+    public Vector3 playerSpawnPosition = new Vector3 (0, 1, -7);
+
+    public Character[] characters;
+
+    public GameObject characterSelectPanel;
+    public GameObject playerSpecificStats;
+
+    public void StartGame(int characterChoice)
+    {
+        characterSelectPanel.SetActive(false);
+        //TODO activev stats panel.
+        playerSpecificStats.SetActive(true);
+        GameObject spawnedPlayer = Instantiate(player, playerSpawnPosition, Quaternion.identity) as GameObject;
+
+        //Index 0 will be the first Character e.g. Warrior
+        Character selectedCharacter = characters[characterChoice]; 
+
+
+    }
+
+}
