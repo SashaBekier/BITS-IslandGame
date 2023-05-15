71bbbf8991ade51236bdfbb91520b944c3000f14
Student-JamesGregory <122055131+Student-JamesGregory@users.noreply.github.com>
2023-05-15 14:49:03 +1000

    altered stats bar
    Changed positioning, made them only update when told
    added function to update them

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,7 +6,6 @@ public class HealthBar : MonoBehaviour
     public GameObject warrior; // Reference to the Warrior object
     public Image healthBar; // Reference to the health bar image
     public RectTransform healthBarTransform; // Reference to the RectTransform component attached to the health bar image
-
     private float maxHealth; // The maximum health of the Warrior
     private float currentHealth; // The current health of the Warrior
 
@@ -15,12 +14,16 @@ public class HealthBar : MonoBehaviour
         maxHealth = warrior.GetComponent<PlayerStats>().HealthTotal; // Get the maximum health from the PlayerStats script attached to the Warrior object
     }
 
-    void Update()
+    // Call this method to update the health bar
+    public void UpdateHealthBar()
     {
-        currentHealth = warrior.GetComponent<PlayerStats>().currentHealth; // Get the current health from the PlayerStats script attached to the Warrior object
-        healthBar.fillAmount = currentHealth / maxHealth; // Set the fill amount of the health bar image based on the current and maximum health values
+        currentHealth = warrior.GetComponent<PlayerStats>().currentHealth;
+        healthBar.fillAmount = currentHealth / maxHealth;
 
-        // Move the health bar to the left based on the fill amount
-        healthBarTransform.offsetMax = new Vector2(-(1 - healthBar.fillAmount) * healthBarTransform.rect.width, healthBarTransform.offsetMax.y);
+        // Shrink the health bar from both ends based on the fill amount
+        float barWidth = healthBarTransform.rect.width;
+        float shrinkAmount = (1 - healthBar.fillAmount) * barWidth / 2;
+        healthBarTransform.offsetMax = new Vector2(-shrinkAmount, healthBarTransform.offsetMax.y);
+        healthBarTransform.offsetMin = new Vector2(shrinkAmount, healthBarTransform.offsetMin.y);
     }
 }
--- Assets/Scripts/MagicBar.cs
+++ Assets/Scripts/MagicBar.cs
@@ -4,23 +4,27 @@ using UnityEngine.UI;
 public class MagicBar : MonoBehaviour
 {
     public GameObject warrior; // Reference to the Warrior object
-    public Image magicBar; // Reference to the health bar image
-    public RectTransform magicBarTransform; // Reference to the RectTransform component attached to the health bar image
-
-    private float maxMagic; // The maximum health of the Warrior
-    private float currentMagic; // The current health of the Warrior
+    public Image magicBar; // Reference to the magic bar image
+    public RectTransform magicBarTransform; // Reference to the RectTransform component attached to the magic bar image
+    private float maxMagic; // The maximum magic of the Warrior
+    private float currentMagic; // The current magic of the Warrior
 
     void Start()
     {
-        maxMagic = warrior.GetComponent<PlayerStats>().MagicTotal; // Get the maximum health from the PlayerStats script attached to the Warrior object
+        maxMagic = warrior.GetComponent<PlayerStats>().MagicTotal; // Get the maximum magic from the PlayerStats script attached to the Warrior object
+        Debug.Log("Max Magic: " + maxMagic);
     }
 
-    void Update()
+    // Call this method to update the magic bar
+    public void UpdateMagicBar()
     {
-        currentMagic = warrior.GetComponent<PlayerStats>().currentMagic; // Get the current health from the PlayerStats script attached to the Warrior object
-        magicBar.fillAmount = currentMagic / maxMagic; // Set the fill amount of the health bar image based on the current and maximum health values
+        currentMagic = warrior.GetComponent<PlayerStats>().currentMagic;
+        magicBar.fillAmount = currentMagic / maxMagic;
 
-        // Move the health bar to the left based on the fill amount
-        magicBarTransform.offsetMax = new Vector2(-(1 - magicBar.fillAmount) * magicBarTransform.rect.width, magicBarTransform.offsetMax.y);
+        // Shrink the magic bar from both ends based on the fill amount
+        float barWidth = magicBarTransform.rect.width;
+        float shrinkAmount = (1 - magicBar.fillAmount) * barWidth / 2;
+        magicBarTransform.offsetMax = new Vector2(-shrinkAmount, magicBarTransform.offsetMax.y);
+        magicBarTransform.offsetMin = new Vector2(shrinkAmount, magicBarTransform.offsetMin.y);
     }
 }
